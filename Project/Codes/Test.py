49654afb83258338788216b1c353c11a56daaf10
Ahmet Talha Sivrikaya <ahmettalhasvrky@gmail.com>
2026-04-28 22:42:38 +0200

    Energy Save Mode Test Added

--- Project/Codes/Test.py
+++ Project/Codes/Test.py
@@ -7,5 +7,14 @@ class TestThermostat(unittest.TestCase):
         current_state = control.get_current_state()
         self.assertEqual(current_state, "Idle")
 
+    def test_energy_saving_mode_when_empty(self):
+        controller = ThermostatController()       
+        controller.set_human_count(0)
+        controller.monitor_occupancy() 
+        self.assertEqual(controller.get_current_state(), "Energy Saving")    
+
+
+
+
 if __name__ == '__main__':
-    unittest.main()
+    unittest.main()        
