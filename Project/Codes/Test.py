cea18a69c0acb64d33be03b3893e5772d5d3cc32
Ahmet Talha Sivrikaya <ahmettalhasvrky@gmail.com>
2026-04-28 22:49:50 +0200

    Dynamic Temp Adjust test Added

--- Project/Codes/Test.py
+++ Project/Codes/Test.py
@@ -13,7 +13,22 @@ class TestThermostat(unittest.TestCase):
         controller.monitor_occupancy() 
         self.assertEqual(controller.get_current_state(), "Energy Saving")    
 
-
+    def test_target_temp_adjusts_with_occupancy(self):
+        # Arrange
+        controller = ThermostatController()
+        
+        # Act: Evde 1 kişi varken 22 derece ideal olsun
+        controller.set_human_count(1)
+        controller.auto_temp_adjustment()
+        temp_with_one = controller.target_temp
+        
+        # Evde 5 kişi varken (insan ısısı arttığı için) hedef sıcaklık düşmeli
+        controller.set_human_count(5)
+        controller.auto_temp_adjustment()
+        temp_with_five = controller.target_temp
+        
+        # Assert: 5 kişi varken hedef sıcaklık daha düşük olmalı
+        self.assertTrue(temp_with_five < temp_with_one)
 
 
 if __name__ == '__main__':
