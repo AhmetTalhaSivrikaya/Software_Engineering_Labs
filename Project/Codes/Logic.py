ab6d9b402cc67cb92d170638de0da7d6a5b90302
SeyitAkdemir <eminseyyidhan@gmail.com>
2026-04-28 22:46:17 +0200

    Logic updated for test

--- Project/Codes/Logic.py
+++ Project/Codes/Logic.py
@@ -1,4 +1,15 @@
 class ThermostatController:
+    def __init__(self):
+        self._state = "Idle"
+        self._human_count = 0
+
     def get_current_state(self):
-        # Testin geçmesi için gereken minimum kod: "Idle" döndürmek
-        return "Idle"
+        return self._state
+
+    def set_human_count(self, count):
+        self._human_count = count
+
+    def monitor_occupancy(self):
+        # TDD "Green" aşaması: Testin beklediği durumu sağlayan minimum mantık
+        if self._human_count == 0:
+            self._state = "Energy Saving"
